b069c5b8036e2ef328bbb1a2b614af432734b523
Cade Brown <brown.cade@gmail.com>
2016-01-08 18:49:19 -0500

    Changed TalonSRX

--- src/org/usfirst/frc/team3966/robot/subsystems/DriveMotor.java
+++ src/org/usfirst/frc/team3966/robot/subsystems/DriveMotor.java
@@ -1,8 +1,8 @@
 package org.usfirst.frc.team3966.robot.subsystems;
 
-import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.TalonSRX;
 
-public class DriveMotor extends Talon{
+public class DriveMotor extends TalonSRX {
   public DriveMotor(int channel) {
     super(channel);
   }
